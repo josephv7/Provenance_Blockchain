4f137a27e8dbf6d10786840fdc4a68f6242c086e
iamjosephvarghese <josephvarghese.rms@gmail.com>
2020-02-10 14:25:25 +0530

    :sparkles: Basic get api working

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const url = require('url');
+const querystring = require('querystring');
+const Request = require("request");
+const axios = require('axios');
+const cors = require('cors')
+
+let app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', function(req, res){
+
+    console.log("Basic GET API");
+
+
+});
+
+
+
+let server = app.listen(4000, function() {
+    console.log('Server is listening on port 4000')
+});
