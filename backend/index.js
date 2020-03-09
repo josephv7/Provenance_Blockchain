7488a70896fe0d855a2ea659bf8d2b1597bb8fee
iamjosephvarghese <josephvarghese.rms@gmail.com>
2020-03-09 13:08:24 +0530

    :sparkles: Auto increment for manufacturer id added

--- backend/index.js
+++ backend/index.js
@@ -137,29 +137,45 @@ app.get('/createManufacturer', function (req,res) {
 
 
     var count;
-    count = 2;
 
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-    Request.post({
-        "headers": { "content-type": "application/json" },
-        "url": "http://localhost:3000/api/Manufacturer",
-        "body": JSON.stringify({
-            "manufacturerName" : req.query.manufacturerName,
-            "participantId" : count.toString(),
-            "participantType" : "manufacturer"
 
-        })
-    }, (error, response, body) => {
-        if(error) {
-            return console.dir(error);
-        }
-        console.dir(JSON.parse(body));
-        res.end(JSON.stringify({ status: "ok" }));
+    axios.get('http://localhost:3000/api/Manufacturer').then(function (response){
+        console.log(response.data);
+        jsonResponse = response.data;
+
+
+    }).then(function (response){
+        findManufacturerCount();
+    }).catch(function (error) {
+        console.log(error);
     });
 
+    function findManufacturerCount() {
+
+        count = 2001 + jsonResponse.length;
+
+        Request.post({
+            "headers": { "content-type": "application/json" },
+            "url": "http://localhost:3000/api/Manufacturer",
+            "body": JSON.stringify({
+                "manufacturerName" : req.query.manufacturerName,
+                "participantId" : count.toString(),
+                "participantType" : "manufacturer"
+
+            })
+        }, (error, response, body) => {
+            if(error) {
+                return console.dir(error);
+            }
+            console.dir(JSON.parse(body));
+            res.end(JSON.stringify({ status: "ok" }));
+        });
+
+    }
 
 
 });
