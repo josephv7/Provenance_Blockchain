3e8e1ccbb4cdc29dc700968206e4b840710ed251
iamjosephvarghese <josephvarghese.rms@gmail.com>
2020-03-09 08:03:29 +0530

    :construction: Added logic for assetTrasnfer event

---
+++ provenance-chain/lib/logic.js
@@ -0,0 +1,25 @@
+/**
+ * Transaction processor function.
+ * @param {org.example.mynetwork.TransferAsset} tx The sample transaction instance.
+ * @transaction
+ */
+
+ async function assetTransfer(tx) {
+     const oldOwnerId = tx.asset.ownerId;
+     tx.asset.ownerId = tx.newOwnerId;
+
+     // Get the asset registry for the asset.
+    const assetRegistry = await getAssetRegistry('org.example.mynetwork.Vehicle');
+    // Update the asset in the asset registry.
+    await assetRegistry.update(tx.asset);
+
+
+    // Emit an event for the modified asset.
+    let event = getFactory().newEvent('org.example.mynetwork', 'AssetTransferEvent');
+    event.asset = tx.asset;
+    event.oldOwnerId = oldOwnerId;
+    event.newOwnerId = tx.newOwnerId;
+    emit(event);
+
+
+ }
