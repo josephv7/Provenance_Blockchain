f3705c8e6a4d66cef82dfbba8b80c1af2e1a8f8c
iamjosephvarghese <josephvarghese.rms@gmail.com>
2020-03-21 14:17:12 +0530

    :sparkles: Auth added for user

--- backend/index.js
+++ backend/index.js
@@ -126,6 +126,57 @@ app.get('/createManufacturer', function (req,res) {
 });
 
 
+
+app.post('/api/userLogin', function (req,res) {
+
+    var userId = req.body.userId;
+    var password = req.body.password;
+    var userType = req.body.userType;
+    var url;
+
+    if(userType == "customer"){
+        url = url = 'http://localhost:3000/api/Customer/' + userId.toString();
+    }else if(userType == "manufacturer"){
+        url = url = 'http://localhost:3000/api/Manufacturer/' + userId.toString();
+    }
+
+
+    axios.get(url).then(function (response){
+        // console.log(response.data);
+        jsonResponse = response.data;
+
+
+    }).then(function (response){
+        var response2 = jsonResponse;
+        checkPassword(response2)
+    }).catch(function (error) {
+        console.log(error);
+        // send invalid id message here
+        res.end(JSON.stringify({ status: "error" }));
+    });
+
+
+    function checkPassword(response){
+        //   console.log(response);
+        console.log(response.password);
+        console.log('inside check');
+
+        if(SHA256(password) == response.password){
+            res.end(JSON.stringify([{ status: "ok" }]));
+            console.log('here');
+        }else{
+            res.end(JSON.stringify([{ status: "incorrect" }]));
+        }
+
+
+    }
+
+
+
+});
+
+
+
 app.get('/createVehicle', function (req,res) {
 
 
@@ -161,6 +212,7 @@ app.get('/createVehicle', function (req,res) {
 
 
 });
+
 
 
 app.get('/listVehicles', function (req,res) {
