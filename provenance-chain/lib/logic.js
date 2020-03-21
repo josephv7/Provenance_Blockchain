53975ba8c2af07b41b089c12683ba1c76faa5688
iamjosephvarghese <josephvarghese.rms@gmail.com>
2020-03-21 15:47:08 +0530

    :ambulance: Error

--- provenance-chain/lib/logic.js
+++ provenance-chain/lib/logic.js
@@ -8,6 +8,13 @@
      const oldOwnerId = tx.asset.ownerId;
      tx.asset.ownerId = tx.newOwnerId;
 
+     const oldOwnerList = tx.asset.ownerList;
+     console.log('oldOwnerList' + oldOwnerList);
+     var tempNewList = tx.asset.ownerList + ',' + tx.newOwnerList.toString();
+     var newList = tempNewList.split(',');
+     console.log('newOwnerList' + newList);
+     tx.asset.ownerList = newList;
+
      // Get the asset registry for the asset.
     const assetRegistry = await getAssetRegistry('org.example.mynetwork.Vehicle');
     // Update the asset in the asset registry.
@@ -19,6 +26,8 @@
     event.asset = tx.asset;
     event.oldOwnerId = oldOwnerId;
     event.newOwnerId = tx.newOwnerId;
+    event.oldOwnerList = oldOwnerList;
+    event.newOwnerId = newList
     emit(event);
 
 
