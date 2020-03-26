c5dde2cfc1fdb642eb0ef9b865afd86b25b3abb3
iamjosephvarghese <josephvarghese.rms@gmail.com>
2020-03-26 12:02:38 +0530

    :sparkles: Added auth controller

--- backend/index.js
+++ backend/index.js
@@ -46,49 +46,6 @@ app.get('/createManufacturer', function (req, res) {
 
 app.post('/api/userLogin', function (req, res) {
 
-    var userId = req.body.userId;
-    var password = req.body.password;
-    var userType = req.body.userType;
-    var url;
-
-
-    if (userType == "customer") {
-        url = blockchainBaseURL + 'Customer/' + userId.toString();
-    } else if (userType == "manufacturer") {
-        url = blockchainBaseURL + 'Manufacturer/' + userId.toString();
-    }
-
-
-    axios.get(url).then(function (response) {
-        // console.log(response.data);
-        jsonResponse = response.data;
-
-
-    }).then(function (response) {
-        var response2 = jsonResponse;
-        checkPassword(response2)
-    }).catch(function (error) {
-        console.log(error);
-        // send invalid id message here
-        res.end(JSON.stringify({status: "error"}));
-    });
-
-
-    function checkPassword(response) {
-        //   console.log(response);
-        console.log(response.password);
-        console.log('inside check');
-
-        if (SHA256(password) == response.password) {
-            res.end(JSON.stringify([{status: "ok"}]));
-            console.log('here');
-        } else {
-            res.end(JSON.stringify([{status: "incorrect"}]));
-        }
-
-
-    }
-
 
 });
 
