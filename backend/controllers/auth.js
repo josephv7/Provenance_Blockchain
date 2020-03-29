1ff04f57275f8a681f7a45095f40817731d26eb1
iamjosephvarghese <josephvarghese.rms@gmail.com>
2020-03-29 23:52:42 +0530

    :zap: Infering userType from first character of id

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -8,14 +8,17 @@ module.exports = {
 
         var userId = req.body.userId;
         var password = req.body.password;
-        var userType = req.body.userType;
+        var userType;
         var url;
 
+        console.log(userId.charAt(0));
 
-        if (userType == "customer") {
+        if (userId.charAt(0) == "2") {
             url = constants.blockchainBaseURL + 'Customer/' + userId.toString();
-        } else if (userType == "manufacturer") {
+            userType = "customer";
+        } else if (userId.charAt(0) == "3") {
             url = constants.blockchainBaseURL + 'Manufacturer/' + userId.toString();
+            userType = "manufacturer";
         }
 
 
@@ -40,7 +43,7 @@ module.exports = {
             console.log('inside check');
 
             if (SHA256(password) == response.password) {
-                res.end(JSON.stringify([{status: "ok", userType: req.body.userType}]));
+                res.end(JSON.stringify([{status: "ok", userType: userType}]));
                 console.log('here');
             } else {
                 res.end(JSON.stringify([{status: "incorrect"}]));
