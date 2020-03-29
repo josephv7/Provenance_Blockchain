99ea9cf18416ee6f5d9fbe3a223f47915e419d5f
mojojojo20 <menonk484@gmail.com>
2020-03-29 22:14:24 +0530

    :boom: switch to remixIcons

--- provchain-react/src/adminRoutes.js
+++ provchain-react/src/adminRoutes.js
@@ -7,53 +7,55 @@ import ListVehicles from "views/listVehicles";
 import ListManufacturer from "views/listManufacturer";
 import OwnerChange from "views/ownerChange";
 
+import 'remixicon/fonts/remixicon.css'
+
 var AdminRoute = [
   {
     path: "/dashboard",
     name: "Dashboard",
-    icon: "ni ni-tv-2 text-primary",
+    icon: "ri-dashboard-line text-primary",
     component: Dashboard,
     layout: "/admin"
   },
   {
     path: "/ownerChange",
     name: "Owner Change",
-    icon: "ni ni-single-02 text-red",
+    icon: "ri-swap-box-line text-red",
     component: OwnerChange,
     layout: "/admin"
   },
   {
     path: "/createConsumer",
     name: "Create Consumer",
-    icon: "ni ni-single-02 text-yellow",
+    icon: "ri-user-add-line text-primary",
     component: CreateConsumer,
     layout: "/admin"
   },
   {
     path: "/createManufactrer",
     name: "Create Manufacturer",
-    icon: "ni ni-single-02 text-yellow",
+    icon: "ri-user-add-line text-primary",
     component: CreateManufacturer,
     layout: "/admin"
   },
   {
     path: "/listCustomers",
     name: "List Customers",
-    icon: "ni ni-bullet-list-67 text-red",
+    icon: "ri-file-list-2-line text-red",
     component: ListCustomer,
     layout: "/admin"
   },
   {
     path: "/listManufacturers",
     name: "List Manufacturers",
-    icon: "ni ni-bullet-list-67 text-red",
+    icon: "ri-file-list-2-line text-red",
     component: ListManufacturer,
     layout: "/admin"
   },
   {
     path: "/listVehicles",
     name: "List Vehicles",
-    icon: "ni ni-bullet-list-67 text-red",
+    icon: "ri-file-list-2-line text-red",
     component: ListVehicles,
     layout: "/admin"
   }
