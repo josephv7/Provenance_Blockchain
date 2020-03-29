3ba09e0684205de28473dcc92c1bc405b1b0b968
mojojojo20 <menonk484@gmail.com>
2020-03-29 23:34:02 +0530

    :boom: auth working

--- provchain-react/src/views/Login.js
+++ provchain-react/src/views/Login.js
@@ -1,5 +1,7 @@
 
 import React from "react";
+import ReactDOM from "react-dom";
+import { Redirect } from 'react-router-dom';
 
 // reactstrap components
 import {
@@ -18,8 +20,61 @@ import {
   Col
 } from "reactstrap";
 import Header from "components/Headers/Header.js";
+import axios from "axios";
+import {nodeURL} from "components/variables";
 
 class Login extends React.Component {
+    state = {
+      userId: '',
+      password: '',
+      userType: ''
+    }
+  nameHandleChange = event => {
+    console.log("name change called")
+    this.setState({ userId: event.target.value });
+  }
+  passwordHandleChange = event => {
+    console.log("password change called")
+    this.setState({ password: event.target.value });
+  }
+  typeHandleChange = event => {
+    console.log("password change called")
+    this.setState({ userType: event.target.value });
+  }
+  
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log("submit called")
+    const { history } = this.props;
+    const auth = {
+      userId: this.state.userId,
+      password: this.state.password,
+      userType: this.state.userType
+    }
+    console.log("name"+auth.userId)
+    
+    // axios.get(nodeURL+"/api/userLogin?username="+auth.username+"&password="+auth.password+"&userType="+auth.userType);
+    axios.post(nodeURL+`/api/userLogin`, 
+      { headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS',}},
+      { data: auth})
+      .then(res => {
+        // console.log(JSON.stringify(res.data));
+        console.log(res.data["status"]);
+        console.log(JSON.stringify(res.data[0].status))
+        if(JSON.stringify(res.data[0].status)=="\"ok\""){
+          history.push('/admin/dashboard');
+        }
+        else{
+            console.log("else")
+        }
+      })
+      .catch(function (error) {
+        console.log("error from catch"+error);
+      })    
+  };
   render() {
     return (
       <>
@@ -35,7 +90,7 @@ class Login extends React.Component {
                 <h2 className="text-muted text-center">ProvChain Auth</h2>
               </CardHeader>
               <CardBody className="px-lg-5 py-lg-5">
-                <Form role="form">
+                <Form role="form" onSubmit={this.handleSubmit}>
                   <FormGroup className="mb-3">
                     <InputGroup className="input-group-alternative">
                       <InputGroupAddon addonType="prepend">
@@ -43,7 +98,7 @@ class Login extends React.Component {
                           <i className="ni ni-email-83" />
                         </InputGroupText>
                       </InputGroupAddon>
-                      <Input placeholder="Username" type="text" autoComplete="new-email"/>
+                      <Input placeholder="Username" type="text" autoComplete="new-email" onChange={this.nameHandleChange}/>
                     </InputGroup>
                   </FormGroup>
                   <FormGroup>
@@ -53,11 +108,21 @@ class Login extends React.Component {
                           <i className="ni ni-lock-circle-open" />
                         </InputGroupText>
                       </InputGroupAddon>
-                      <Input placeholder="Password" type="password" autoComplete="new-password"/>
+                      <Input placeholder="Password" type="password" onChange={this.passwordHandleChange}/>
+                    </InputGroup>
+                  </FormGroup>
+                  <FormGroup>
+                    <InputGroup className="input-group-alternative">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="ni ni-lock-circle-open" />
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input placeholder="User Type" type="text" onChange={this.typeHandleChange}/>
                     </InputGroup>
                   </FormGroup>
                   <div className="text-center">
-                    <Button className="my-4" color="primary" type="button">
+                    <Button className="my-4" color="primary" type="submit">
                       Sign in
                     </Button>
                   </div>
