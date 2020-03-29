066129f92315ed5b2ef76beae6ee5d1f2da8e4e0
mojojojo20 <menonk484@gmail.com>
2020-03-29 21:18:17 +0530

    :sparkles: fetch by id for vehicles

--- provchain-react/src/views/listVehicles.js
+++ provchain-react/src/views/listVehicles.js
@@ -5,20 +5,50 @@ import {
   Card,
   CardHeader,
   CardFooter,
+  CardBody,
+  Form,
+  FormGroup,
+  Input,
+  Button,
+  Col,
+  Modal,
   Table,
   Container,
   Row,
 } from "reactstrap";
 import Header from "components/Headers/Header.js";
 
-import {nodeURL} from "components/variables"
+import {nodeURL} from "components/variables";
+import axios from "axios";
 class ListVehicles extends React.Component {
   constructor(){
     super();
     this.state = {
-      apiData : {}
+      apiData : {},
+      exampleModal: false,
+      vehicleIdDetails: '',
+      fetchId: ''
     }
   }
+  toggleModal(id){
+    if(this.state.exampleModal==false){
+      console.log("fetch by id")
+      axios.get("https://d5911af3.ngrok.io/api/Vehicle/"+id)
+      .then(res => {
+        // const vehicleIdDetails = res.data;
+        this.setState({ vehicleIdDetails: res.data });
+        console.log("by id: "+this.state.vehicleIdDetails)
+      })
+    }
+    this.setState({
+      exampleModal: !this.state.exampleModal
+    });
+  };
+  fetchById = event => {
+    console.log("password change called")
+    this.setState({ fetchId: event.target.value });
+  }
+  
   // use after POST or while fetch by ID
   fetchData(){
     fetch(nodeURL+'/listVehicles')
@@ -44,8 +74,96 @@ class ListVehicles extends React.Component {
     return (
       <>
         <Header />
+        <Modal
+          className="modal-dialog-centered"
+          isOpen={this.state.exampleModal}
+          toggle={() => this.toggleModal("exampleModal")}
+        >
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">
+              Vehicle Details
+            </h5>
+            <button
+              aria-label="Close"
+              className="close"
+              data-dismiss="modal"
+              type="button"
+              onClick={() => this.toggleModal("exampleModal")}
+            >
+              <span aria-hidden={true}>×</span>
+            </button>
+          </div>
+          <div className="modal-body">
+             <h5>Plate Number: {JSON.stringify(this.state.vehicleIdDetails["plateNumber"])}</h5>
+             <h5>Manufacturer Name : {JSON.stringify(this.state.vehicleIdDetails["manufacturer"])}</h5>
+             <h5>Owner ID : {JSON.stringify(this.state.vehicleIdDetails["ownerId"])}</h5>
+          </div>
+          <div className="modal-footer">
+            <Button
+              color="secondary"
+              data-dismiss="modal"
+              type="button"
+              onClick={() => this.toggleModal("exampleModal")}
+            >
+              Close
+            </Button>
+          </div>
+        </Modal>
         <Container className="mt--7" fluid>
-          <Row>
+        <Row>
+            <Col md={5} xs={12}>
+                    <div className="col">
+                        <Card className="shadow">
+                          <CardHeader>
+                            <h3 className="mb-0">List Vehicle By Chassis Number</h3>
+                          </CardHeader>
+                          <CardBody>
+                          <Form>
+                              <h6 className="heading-small text-muted mb-4">
+                                List Vehicle Details wrt Chassis Number
+                              </h6>
+                              <div className="pl-lg-1">
+                                
+                                  <Col lg="12">
+                                    <FormGroup>
+                                      <label
+                                        className="form-control-label"
+                                        htmlFor="input-username"
+                                      >
+                                        Chassis Number
+                                      </label>
+                                      <Input
+                                        className="form-control-alternative"                
+                                        id="input-username"
+                                        placeholder="Enter Chassis Number"
+                                        type="text"
+                                        name = "customerName"
+                                        onChange = {this.fetchById}
+                                      />
+                                    </FormGroup>
+                                  </Col>
+                                
+                                <hr className="my-4" />
+                                <Row>
+                                  <Col className="text-right" xs="12">
+                                  <Button
+                                    color="success"              
+                                    // type="submit"
+                                    size="lg"
+                                    onClick={() => this.toggleModal(this.state.fetchId)}
+                                  >
+                                    Submit
+                                  </Button>
+                              </Col>
+                                </Row>
+                              </div>
+                              
+                            </Form>
+                            </CardBody>
+                        </Card>
+                      </div>
+              </Col>
+              <Col xs={12} md={7}>
             <div className="col">
               <Card className="shadow">
                 <CardHeader className="border-0">
@@ -75,6 +193,7 @@ class ListVehicles extends React.Component {
                 </Table>
               </Card>
             </div>
+            </Col>
           </Row>
 
         </Container>
