73245636787d51d118b9ab648e0f7589173c5572
iamjosephvarghese <josephvarghese.rms@gmail.com>
2020-03-30 23:46:47 +0530

    :zap: Added manufacturer layout

---
+++ provchain-react/src/layouts/Manufacturer.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import AdminNavbar from "components/Navbars/AdminNavbar.js";
+import Sidebar from "components/Sidebar/Sidebar.js";
+
+import routes from "../routes/manufacturerRoutes";
+
+class Manufacturer extends React.Component{
+    componentDidUpdate(e) {
+        document.documentElement.scrollTop = 0;
+        document.scrollingElement.scrollTop = 0;
+        this.refs.mainContent.scrollTop = 0;
+    }
+    getRoutes = routes => {
+        return routes.map((prop, key) => {
+            if (prop.layout === "/manufacturer") {
+                return (
+                    <Route
+                        path={prop.layout + prop.path}
+                        component={prop.component}
+                        key={key}
+                    />
+                );
+            } else {
+                return null;
+            }
+        });
+    };
+    getBrandText = path => {
+        for (let i = 0; i < routes.length; i++) {
+            if (
+                this.props.location.pathname.indexOf(
+                    routes[i].layout + routes[i].path
+                ) !== -1
+            ) {
+                return routes[i].name;
+            }
+        }
+        return "Brand";
+    };
+    render() {
+        return (
+            <>
+                <Sidebar
+                    {...this.props}
+                    routes={routes}
+                    logo={{
+                        innerLink: "/manufacturer/index",
+                        imgSrc: require("assets/img/brand/logo-png.png"),
+                        imgAlt: "..."
+                    }}
+                />
+                <div className="main-content" ref="mainContent">
+                    <AdminNavbar
+                        {...this.props}
+                        brandText={this.getBrandText(this.props.location.pathname)}
+                    />
+                    <Switch>
+                        {this.getRoutes(routes)}
+                        <Redirect from="*" to="/manufacturer/index" />
+                    </Switch>
+                </div>
+            </>
+        );
+    }
+}
+
+export default Manufacturer;
