0d82ae7ffe2b4903692bc00d3f39edf5dd20525a
iamjosephvarghese <josephvarghese.rms@gmail.com>
2020-04-06 00:23:59 +0530

    :sparkles: Adding dealer routes and layout

---
+++ provchain-react/src/routes/dealerRoutes.js
@@ -0,0 +1,46 @@
+
+import Dashboard from "../views/Dashboard";
+import CreateConsumer from "../views/createConsumer";
+import ListCustomer from "../views/listConsumers";
+import ListVehicles from "../views/listVehicles";
+import CreateVehicle from "../views/createVehicle";
+import 'remixicon/fonts/remixicon.css'
+
+var DealerRoute = [
+    {
+        path: "/dashboard",
+        name: "Dashboard",
+        icon: "ri-dashboard-line text-primary",
+        component: Dashboard,
+        layout: "/dealer"
+    },
+    {
+        path: "/createConsumer",
+        name: "Create Consumer",
+        icon: "ri-user-add-line text-primary",
+        component: CreateConsumer,
+        layout: "/dealer"
+    },
+    {
+        path: "/createVehicle",
+        name: "Create Vehicle",
+        icon: "ri-user-add-line text-primary",
+        component: CreateVehicle,
+        layout: "/dealer"
+    },
+    {
+        path: "/listCustomers",
+        name: "List Customers",
+        icon: "ri-file-list-2-line text-red",
+        component: ListCustomer,
+        layout: "/dealer"
+    },
+    {
+        path: "/listVehicles",
+        name: "List Vehicles",
+        icon: "ri-file-list-2-line text-red",
+        component: ListVehicles,
+        layout: "/dealer"
+    }
+];
+export default DealerRoute;
