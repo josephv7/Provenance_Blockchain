5af262db59620bdf62b911158b6b0f69eeb455ef
iamjosephvarghese <josephvarghese.rms@gmail.com>
2020-04-07 23:56:59 +0530

    :zap: Made a common handleChange function, added new field plateNumber to state

--- provchain-react/src/views/ownerChange.js
+++ provchain-react/src/views/ownerChange.js
@@ -1,20 +1,19 @@
-
 import React from "react";
 
 // reactstrap components
 import {
-  Button,
-  Card,
-  CardHeader,
-  CardBody,
-  FormGroup,
-  Form,
-  Modal,
-  Spinner,
-  Input,
-  Container,
-  Row,
-  Col
+    Button,
+    Card,
+    CardHeader,
+    CardBody,
+    FormGroup,
+    Form,
+    Modal,
+    Spinner,
+    Input,
+    Container,
+    Row,
+    Col
 } from "reactstrap";
 
 // core components
@@ -26,216 +25,219 @@ import axios from "axios"
 
 
 class OwnerChange extends React.Component {
-  state = {
-    chassisNumber: '',
-    newOwnerId: '',
-    exampleModal: false,
-    loading: false
-  }
-  toggleModal(){
-    this.setState({
-      exampleModal: !this.state.exampleModal
-    });
-  };
-  nameHandleChange = event => {
-    console.log("name change called")
-    this.setState({ chassisNumber: event.target.value });
-  }
-  passwordHandleChange = event => {
-    console.log("password change called")
-    this.setState({ newOwnerId: event.target.value });
-  }
-  
-  handleSubmit = event => {
-    event.preventDefault();
-    console.log("submit called")
-  
-    const vehicle = {
-      chassisNumber: this.state.chassisNumber,
-      newOwnerId: this.state.newOwnerId 
+    state = {
+        chassisNumber: '',
+        newOwnerId: '',
+        plateNumber: '',
+        exampleModal: false,
+        loading: false
     }
-    this.setState({loading: true})
-    axios.get(nodeURL+"/ownerChangeRequest",{
-      params : {
-        chassisNumber : vehicle.chassisNumber,
-        newOwnerId : vehicle.newOwnerId
-      }
-    })
-    .then(res => {
-      console.log(res)
-      console.log(res.data.status)
-      if(res.data.status=="ok"){
-            this.toggleModal();
-            this.setState({loading: false})
-      }
-    })
-    // axios.post(nodeURL+`/ownerChange`, 
-    //   { headers: {
-    //             "Content-Type": "application/json",
-    //             "Access-Control-Allow-Origin": "*",
-    //             'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS',}},
-    //   { data: vehicle})
-    //   .then(res => {
-    //     console.log(res);
-    //   })
-    //   .catch(function (error) {
-    //     console.log("error from catch"+error);
-    //   })    
-  };
-  render() {
-    return (
-      <>
-        <Header />
-        <Modal
-          className="modal-dialog-centered"
-          isOpen={this.state.exampleModal}
-          toggle={() => this.toggleModal("exampleModal")}
-        >
-          <div className="modal-header">
-            <h2 className="modal-title" id="exampleModalLabel">
-              Success
-            </h2>
-            <button
-              aria-label="Close"
-              className="close"
-              data-dismiss="modal"
-              type="button"
-              onClick={() => this.toggleModal("exampleModal")}
-            >
-              <span aria-hidden={true}>×</span>
-            </button>
-          </div>
-          <div className="modal-body text-center">
-            <i class="ri-heart-line ri-3x text-success"></i>
-            <h4 class="text-success">Success</h4>
-            <h4 class="text-muted">New Owner is <span class="text-success">{this.state.newOwnerId}</span></h4>
-          </div>
-          <div className="modal-footer">
-            <Button
-              color="secondary"
-              data-dismiss="modal"
-              type="button"
-              onClick={() => this.toggleModal("exampleModal")}
-            >
-              Close
-            </Button>
-          </div>
-        </Modal>
-        <Container className="mt--7" fluid>
-          <Row>
-            <Col className="order-xl-1" xl="10">
-              <Card className="bg-secondary shadow">
-                <CardHeader className="bg-white border-0">
-                  <Row className="align-items-center">
-                    <Col xs="8">
-                      <h3 className="mb-0">Owner Change</h3>
-                    </Col>
-                    <Col className="text-right" xs="4">
-                      <Button
-                        color="primary"
-                        href="#pablo"
-                        onClick={e => e.preventDefault()}
-                        size="sm"
-                      >
-                        Owner
-                      </Button>
-                    </Col>
-                  </Row>
-                </CardHeader>
-                <CardBody>
-                  {this.state.loading ? <Spinner color="dark" /> :
-                  <Form onSubmit = {this.handleSubmit}>
-                    <h6 className="heading-small text-muted mb-4">
-                      Invoke Owner Chagne Event on a Vehicle
-                    </h6>
-                    <div className="pl-lg-4">
-                      <Row>
-                        <Col lg="6">
-                          <FormGroup>
-                            <label
-                              className="form-control-label"
-                              htmlFor="input-username"
-                            >
-                              Chassis Number
-                            </label>
-                            <Input
-                              className="form-control-alternative"                
-                              id="input-username"
-                              placeholder="Chassis Number"
-                              type="text"
-                              onChange = {this.nameHandleChange}
-                            />
-                          </FormGroup>
-                        </Col>
-                        <Col lg="6">
-                          <FormGroup>
-                            <label
-                              className="form-control-label"
-                              htmlFor="input-email"
-                            >
-                              Owner ID 
-                            </label>
-                            <Input
-                              className="form-control-alternative"
-                              id="input-email"
-                              placeholder="New Owner ID"
-                              type="text"
-                              onChange = {this.passwordHandleChange}
-                            />
-                          </FormGroup>
-                        </Col>
-                      </Row>
+
+    toggleModal() {
+        this.setState({
+            exampleModal: !this.state.exampleModal
+        });
+    };
+
+    handleChange = event => {
+        const {name, value} = event.target
+        this.setState({
+            [name]: value
+        })
+    }
 
 
+    handleSubmit = event => {
+        event.preventDefault();
+        console.log("submit called")
 
+        const vehicle = {
+            chassisNumber: this.state.chassisNumber,
+            newOwnerId: this.state.newOwnerId
+        }
+        this.setState({loading: true})
+        axios.get(nodeURL + "/ownerChangeRequest", {
+            params: {
+                chassisNumber: vehicle.chassisNumber,
+                newOwnerId: vehicle.newOwnerId
+            }
+        })
+            .then(res => {
+                console.log(res)
+                console.log(res.data.status)
+                if (res.data.status == "ok") {
+                    this.toggleModal();
+                    this.setState({loading: false})
+                }
+            })
+        // axios.post(nodeURL+`/ownerChange`,
+        //   { headers: {
+        //             "Content-Type": "application/json",
+        //             "Access-Control-Allow-Origin": "*",
+        //             'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS',}},
+        //   { data: vehicle})
+        //   .then(res => {
+        //     console.log(res);
+        //   })
+        //   .catch(function (error) {
+        //     console.log("error from catch"+error);
+        //   })
+    };
 
-                      <Row>
-                        <Col lg="6">
-                          <FormGroup>
-                            <label
-                                className="form-control-label"
-                                htmlFor="input-username"
-                            >
-                              Plate Number
-                            </label>
-                            <Input
-                                name="plateNumber"
-                                className="form-control-alternative"
-                                id="input-username"
-                                placeholder="Chassis Number"
-                                type="text"
-                                onChange = {this.nameHandleChange}
-                            />
-                          </FormGroup>
-                        </Col>
-                      </Row>
-
-
-
-                      <hr className="my-4" />
-                      <Row>
-                        <Col className="text-right" xs="12">
-                        <Button
-                          color="success"              
-                          type="submit"                        
-                          size="lg"
+    render() {
+        return (
+            <>
+                <Header/>
+                <Modal
+                    className="modal-dialog-centered"
+                    isOpen={this.state.exampleModal}
+                    toggle={() => this.toggleModal("exampleModal")}
+                >
+                    <div className="modal-header">
+                        <h2 className="modal-title" id="exampleModalLabel">
+                            Success
+                        </h2>
+                        <button
+                            aria-label="Close"
+                            className="close"
+                            data-dismiss="modal"
+                            type="button"
+                            onClick={() => this.toggleModal("exampleModal")}
                         >
-                          Submit
-                        </Button>
-                    </Col>
-                      </Row>
+                            <span aria-hidden={true}>×</span>
+                        </button>
                     </div>
-                    
-                  </Form>
-                  }
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </>
-    );
-  }
+                    <div className="modal-body text-center">
+                        <i class="ri-heart-line ri-3x text-success"></i>
+                        <h4 class="text-success">Success</h4>
+                        <h4 class="text-muted">New Owner is <span class="text-success">{this.state.newOwnerId}</span>
+                        </h4>
+                    </div>
+                    <div className="modal-footer">
+                        <Button
+                            color="secondary"
+                            data-dismiss="modal"
+                            type="button"
+                            onClick={() => this.toggleModal("exampleModal")}
+                        >
+                            Close
+                        </Button>
+                    </div>
+                </Modal>
+                <Container className="mt--7" fluid>
+                    <Row>
+                        <Col className="order-xl-1" xl="10">
+                            <Card className="bg-secondary shadow">
+                                <CardHeader className="bg-white border-0">
+                                    <Row className="align-items-center">
+                                        <Col xs="8">
+                                            <h3 className="mb-0">Owner Change</h3>
+                                        </Col>
+                                        <Col className="text-right" xs="4">
+                                            <Button
+                                                color="primary"
+                                                href="#pablo"
+                                                onClick={e => e.preventDefault()}
+                                                size="sm"
+                                            >
+                                                Owner
+                                            </Button>
+                                        </Col>
+                                    </Row>
+                                </CardHeader>
+                                <CardBody>
+                                    {this.state.loading ? <Spinner color="dark"/> :
+                                        <Form onSubmit={this.handleSubmit}>
+                                            <h6 className="heading-small text-muted mb-4">
+                                                Invoke Owner Chagne Event on a Vehicle
+                                            </h6>
+                                            <div className="pl-lg-4">
+                                                <Row>
+                                                    <Col lg="6">
+                                                        <FormGroup>
+                                                            <label
+                                                                className="form-control-label"
+                                                                htmlFor="input-username"
+                                                            >
+                                                                Chassis Number
+                                                            </label>
+                                                            <Input
+                                                                className="form-control-alternative"
+                                                                id="input-username"
+                                                                name="chassisNumber"
+                                                                placeholder="Chassis Number"
+                                                                type="text"
+                                                                onChange={this.handleChange}
+                                                            />
+                                                        </FormGroup>
+                                                    </Col>
+                                                    <Col lg="6">
+                                                        <FormGroup>
+                                                            <label
+                                                                className="form-control-label"
+                                                                htmlFor="input-email"
+                                                            >
+                                                                Owner ID
+                                                            </label>
+                                                            <Input
+                                                                className="form-control-alternative"
+                                                                id="input-email"
+                                                                placeholder="New Owner ID"
+                                                                name="newOwnerId"
+                                                                type="text"
+                                                                onChange={this.handleChange}
+                                                            />
+                                                        </FormGroup>
+                                                    </Col>
+                                                </Row>
+
+
+                                                <Row>
+                                                    <Col lg="6">
+                                                        <FormGroup>
+                                                            <label
+                                                                className="form-control-label"
+                                                                htmlFor="input-username"
+                                                            >
+                                                                Plate Number
+                                                            </label>
+                                                            <Input
+                                                                name="plateNumber"
+                                                                className="form-control-alternative"
+                                                                id="input-username"
+                                                                placeholder="Chassis Number"
+                                                                type="text"
+                                                                onChange={this.handleChange}
+                                                            />
+                                                        </FormGroup>
+                                                    </Col>
+                                                </Row>
+
+
+                                                <hr className="my-4"/>
+                                                <Row>
+                                                    <Col className="text-right" xs="12">
+                                                        <Button
+                                                            color="success"
+                                                            type="submit"
+                                                            size="lg"
+                                                        >
+                                                            Submit
+                                                        </Button>
+                                                    </Col>
+                                                </Row>
+                                            </div>
+
+                                        </Form>
+                                    }
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        );
+    }
 }
 
 export default OwnerChange;
