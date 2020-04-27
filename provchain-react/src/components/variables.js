41480620670faebe1f11d171dfaf5c48e125b663
iamjosephvarghese <josephvarghese.rms@gmail.com>
2020-04-27 16:18:20 +0530

    :ambulance: Changed node url back to localhost

--- provchain-react/src/components/variables.js
+++ provchain-react/src/components/variables.js
@@ -1,5 +1,5 @@
-// const nodeURL = "http://localhost:4000"
-const nodeURL = "https://f3d25bbd.ngrok.io"
+const nodeURL = "http://localhost:4000"
+// const nodeURL = "https://f3d25bbd.ngrok.io"
 
 module.exports = {
     nodeURL
